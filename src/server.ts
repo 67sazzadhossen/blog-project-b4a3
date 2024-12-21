c0f4fa47d92c5dd9e93ed71893b526705cff566a
67sazzadhossen <sazzadhossen010@gmail.com>
2024-12-21 23:28:46 +0600

    c

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config, () => {
+    app.listen(config.port, () => {
       console.log(`App listening on port ${config.port}`);
     });
   } catch (error) {
